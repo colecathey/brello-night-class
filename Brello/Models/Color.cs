30ebae457146702c0e8bfecf2dc8526cdeece25f
colecathey <cole.cathey@outlook.com>
2015-10-28 20:12:50 -0500

    update

--- Brello/Models/Color.cs
+++ Brello/Models/Color.cs
@@ -5,10 +5,20 @@ using System.Web;
 
 namespace Brello.Models
 {
-    public class Color
+    public class Color : IComparable
     {
         public int ColorId { get; }
         public string Name { get; set; }
         public string Value { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Color other_color = obj as Color;
+            //Other way to cast
+            //Color other_color = (Color)obj; ***returns null***
+            return this.Name.CompareTo(other_color.Name);
+            
+            //throw new NotImplementedException();
+        }
     }
 }
