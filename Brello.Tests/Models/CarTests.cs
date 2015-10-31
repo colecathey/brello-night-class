d2cffcbfe0f8dca95f97136ddd7a79a9c48e5e5f
colecathey <cole.cathey@outlook.com>
2015-10-31 11:44:08 -0500

    lunch update

---
+++ Brello.Tests/Models/CarTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Models;
+using Moq;
+
+namespace Brello.Tests.Models
+{
+    [TestClass]
+    public class CarTests
+    {
+        [TestMethod]
+        public void CarEnsureICanInstance()
+        {
+            Car mycar = new Car();
+            Assert.IsNotNull(mycar);
+        }
+
+        [TestMethod]
+        public void CarEnsureMyCarCanHonk()
+        {
+            Car mycar = new Car();
+            Assert.AreEqual("HONK!", mycar.Horn());
+        }
+        [TestMethod]
+        public void CarEnsureICanMockMyCarHorn()
+        {
+            Mock<Car> mock_car = new Mock<Car>();
+            mock_car.Setup(x => x.Horn()).Returns("BEEP!");
+            Assert.AreEqual("BEEP!", mock_car.Object.Horn());
+        }
+
+        [TestMethod]
+        public void CarEnsureICanMockInterface()
+        {
+            Mock<ICar> mock_car = new Mock<ICar>();
+            // This is invalid: var myvar = new ICar();
+            mock_car.Setup(x => x.Honk()).Returns("BEEP!");
+            Assert.AreEqual("BEEP!", mock_car.Object.Honk());
+            mock_car.Verify(x => x.Honk()), Times.Once);
+        }
+        //[TestMethod]
+        //public void CarEnsureReadyEngineIsCalled()
+        //{
+        //    Mock<Car> mock_car = new Mock<Car>();
+        //    mock_car.Setup(x => x.Horn()).Returns("BEEP!");
+        //    Assert.AreEqual("BEEP!", mock_car.Object.Horn());
+        //    mock_car.Verify(x => x.ReadyEngines(), Times.Once)
+        //}
+    }
+}
--- Brello/Models/Car.cs
+++ Brello/Models/Car.cs
@@ -5,7 +5,21 @@ using System.Web;
 
 namespace Brello.Models
 {
+
+    public interface ICar
+    {
+        string Honk();
+    }
+
     public class Car
     {
+        // In order for mock to change behavior, Method must be virtual
+        public virtual string Horn()
+        {
+            ReadyEngines();
+            return "HONK!";
+        }
+
+        public void ReadyEngines() { }
     }
 }
